1211f3906612870d996c7dcd4676c9ccedc86a3a
Bogdan Yurov <bogdan@yurov.me>
2016-05-22 21:53:46 +0300

    updated config

--- src/boot.sh
+++ src/boot.sh
@@ -8,13 +8,13 @@ export ETCD=$CONFD_ETCD_NODE
 echo "[nginx] booting container. ETCD: $ETCD"
 
 # Loop until confd has updated the nginx config
-until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/myconfig.toml; do
+until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/nginx.toml; do
   echo "[nginx] waiting for confd to refresh nginx.conf"
   sleep 5
 done
 
 # Run confd in the background to watch the upstream servers
-confd -interval 10 -node $ETCD -config-file /etc/confd/conf.d/myconfig.toml &
+confd -interval 10 -node $ETCD -config-file /etc/confd/conf.d/nginx.toml &
 echo "[nginx] confd is listening for changes on etcd..."
 
 # Start nginx
